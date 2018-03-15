eefe6e0393630be10e31246b8488c308a54dc90a
narender <narender2031@gmail.com>
2018-03-15 17:40:23 +0530

    logs file

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,34 +11,34 @@ set :deploy_to, "/home/#{fetch :deploy_user}/#{fetch :application}"
 #  set :rbenv_path, '/home/deploy/.rbenv/'
 #  set :rbenv_prefix, "/home/deploy/.rbenv/bin/rbenv exec"
 # set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-append :linked_dirs, "config/database.yml", "config/secrets.yml"
+# append :linked_dirs, "config/database.yml", "config/secrets.yml"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 # dirs we want symlinked to the shared folder
 # during deployment
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# namespace :deploy do
+namespace :deploy do
 
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       # execute :touch, release_path.join('tmp/restart.txt')
-#       #
-# 	  # The capistrano-unicorn-nginx gem handles all this
-# 	  # for this example
-#     end
-#   end
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      #
+	  # The capistrano-unicorn-nginx gem handles all this
+	  # for this example
+    end
+  end
 
-#   after :publishing, :restart
+  after :publishing, :restart
 
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
 
-# end
+end
