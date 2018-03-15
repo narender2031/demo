78b022b87d011ff42ba241aff137891277f33839
narender <narender2031@gmail.com>
2018-03-15 17:03:48 +0530

    ds

---
+++ config/unicorn.rb
@@ -0,0 +1,7 @@
+pid "/home/deploy/demo/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/demo/current/unicorn/unicorn.log"
+stdout_path "/home/deploy/demo/current/unicorn/unicorn.log"
+
+listen "/tmp/unicorn.demo.sock"
+worker_processes 2
+timeout 30
