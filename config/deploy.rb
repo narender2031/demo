ffb50d8ff14d31a1a17675af412b882726885224
narender <narender2031@gmail.com>
2018-03-15 19:30:26 +0530

    restat unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,27 +18,24 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-      #
-	  # The capistrano-unicorn-nginx gem handles all this
-	  # for this example
-    end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "if [ -f #{unicorn_pid} ]; then kill -s USR2 `cat #{unicorn_pid}`; fi"
   end
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  task :force_restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
   end
 
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "if [ -f #{unicorn_pid} ]; then kill -s QUIT `cat #{unicorn_pid}`; fi"
+  end
+
+  task :start, :roles => :app do
+    run "cd #{current_path} && #{unicorn_binary}"
+  end
+
+  task :symlink_database, :roles => :app do
+    run "cd #{current_path}/config && ln -s #{current_path}/../../shared/config/database.yml ."
+  end
 end
