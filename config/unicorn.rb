7c6f39d6bb145ad18e9ff7ab59548dc64a99cb3c
narender <narender2031@gmail.com>
2018-03-15 18:34:50 +0530

    not working unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/home/deploy/demo"
-pid "/home/deploy/demo/tmp/pids/unicorn.pid"
-stderr_path "/home/deploy/demo/log/unicorn.log"
-stdout_path "/home/deploy/demo/log/unicorn.log"
+# working_directory "/home/deploy/demo/current"
+pid "/home/deploy/demo/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/demo/current/log/unicorn.log"
+stdout_path "/home/deploy/demo/current/log/unicorn.log"
 
 listen "/tmp/unicorn.demo.sock"
 worker_processes 2
