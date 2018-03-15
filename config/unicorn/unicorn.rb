a0ac5438db76435f35874ee0a6a1a31c3dfd25b9
narender <narender2031@gmail.com>
2018-03-15 15:29:57 +0530

    issue

--- config/unicorn/unicorn.rb
+++ config/unicorn/unicorn.rb
@@ -1,8 +1,8 @@
 rails_root = File.expand_path(‘../../’, __FILE__)
 rails_env = ENV['RAILS_ENV'] || 'production'
 worker_processes 2
-working_directory rails_root
-listen '#{rails_root}/tmp/unicorn.sock'
-pid '#{rails_root}/tmp/unicorn.pid'
-stderr_path '#{rails_root}/log/#{rails_env}_unicorn_error.log'
-stdout_path '#{rails_root}/log/#{rails_env}_unicorn.log'
+working_directory '/home/deploy/demo/current/demo_production/current'
+listen '/home/deploy/demo/current/demo_production/current/tmp/unicorn.sock'
+pid '/home/deploy/demo/current/demo_production/current/tmp/unicorn.pid'
+stderr_path '/home/deploy/demo/current/demo_production/current/log/#{rails_env}_unicorn_error.log'
+stdout_path '/home/deploy/demo/current/demo_production/current/log/#{rails_env}_unicorn.log'
